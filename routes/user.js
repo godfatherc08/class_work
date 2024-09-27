139c4e4035c0530f61e6375c40cbf059026810e2
godfatherc08 <139884797+godfatherc08@users.noreply.github.com>
2024-09-27 20:50:26 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 
 const express = require('express')
 const router = express.Router()
-const { addUser, getUser } = require('../controllers/user')
+const { addUser, getUser, logoutUser, deleteUser } = require('../controllers/user')
 
 router.post('/add-user', addUser)
 router.get('/get-user', getUser)
+router.post('/logout-user', logoutUser)
+router.delete('/delete-user', deleteUser)
 
 
 module.exports = router
