3705bf58bdaa2e90da03dc701f92817f6c68a6b8
godfatherc08 <139884797+godfatherc08@users.noreply.github.com>
2024-09-27 20:49:43 +0100

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
 
+
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const secretKey = "sui1d23kf29g382fgojb"
 
 const addUser = async(req, res) => {
     const { username, emailAddress, password} = req.body
@@ -31,9 +33,13 @@ const getUser = async (req, res) => {
         res.send("User cannot be found")
     }
     if(check){
+        const token = jwt.sign({username: savedUser.username}, secretKey, {expiresIn : '1h'}) 
+        res.cookie('token', token)
+
         res.send("Welcome " + savedUser.username)
         console.log(savedUser)
     }
+    
     else{
         res.send("Invalid credentials")
     }
@@ -42,6 +48,43 @@ const getUser = async (req, res) => {
         console.log(err)
     }
 }
+const logoutUser = async(req, res) =>{
+    const {emailAddress, password} = req.body
+    try{
+    const savedUser = await User.findOne({ emailAddress })
+    const check = bcrypt.compareSync(password, savedUser.password)
+    if(savedUser && check){
+        res.clearCookie('token')
+        res.status(200).send('Logged out')
+    }
+    else{
+        res.send('Wrong credentials')
+    }
+    }
+    catch(err){
+        return res.status(404).send('error logging out')
+    }
+}
+
+const deleteUser = async(req,res) => {
+    const {emailAddress, password} = req.body
+    try{
+        const savedUser = await User.findOne({ emailAddress })
+        const check = bcrypt.compareSync(password, savedUser.password)
+        if(savedUser && check){
+            const UserId = savedUser._id
+            await User.findByIdAndDelete(UserId)
+            res.clearCookie('token')
+            res.status(200).send('Logged out')
+        }
+        else{
+            res.send('Wrong credentials')
+        }
+    }
+    catch(err){
+        res.status(404).send(err)
+    }
+}
 
 
-module.exports = { addUser, getUser }
+module.exports = { addUser, getUser, logoutUser, deleteUser }
